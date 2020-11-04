2dc402cf613f2e2889476e0a284925ff931f1924
z0j0js <783155876@qq.com>
2020-11-04 22:52:39 +0800

    1. mybatis使用实例：http://localhost:9000/system/test 2. 增加三层结构

---
+++ system/src/main/java/com/sise/system/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.sise.system.mapper;
+
+import com.sise.system.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+
+    public List<Test> list();
+}
---
+++ system/src/main/java/com/sise/system/service/TestService.java
@@ -0,0 +1,19 @@
+package com.sise.system.service;
+
+import com.sise.system.domain.Test;
+import com.sise.system.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list() {
+        return testMapper.list();
+    }
+}
