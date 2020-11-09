347dc0b5b4fdb0ff78ebf5f7a41fb6016a02bfc9
z0j0js <783155876@qq.com>
2020-11-09 20:45:17 +0800

    1. 集成mybatis-generator

--- system/src/main/java/com/sise/system/mapper/TestMapper.java
+++ system/src/main/java/com/sise/system/mapper/TestMapper.java
@@ -1,10 +1,31 @@
 package com.sise.system.mapper;
 
 import com.sise.system.domain.Test;
-
+import com.sise.system.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface TestMapper {
+    long countByExample(TestExample example);
 
-    public List<Test> list();
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
+
 }
