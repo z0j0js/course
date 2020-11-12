e2a804f23a40a01d0d85d055ad027f9967c2df52
z0j0js <783155876@qq.com>
2020-11-12 22:59:18 +0800

    1. 增加分页组件pagination.vue 2. 大章管理页面使用分页组件，可自定义初始每页5条，最多显示8个按钮

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <p>
-            <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+            <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
                 <i class="ace-icon fa fa-refresh"></i>
                 刷新
             </button>
         </p>
+        <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
         <table id="simple-table" class="table  table-bordered table-hover">
             <thead>
             <tr>
@@ -82,8 +83,10 @@
 </template>
 
 <script>
+    import Pagination from "../../components/pagination";
     export default {
         name: "chapter",
+        components: {Pagination},
         data: function(){
             return {
                 chapters: []
@@ -91,19 +94,21 @@
         },
         mounted() {
             let _this = this;
-            _this.list();
+            _this.$refs.pagination.size = 5;
+            _this.list(1);
             //this.$parent.activeSidebar("business-chapter-sidebar");
 
         },
         methods: {
-            list() {
+            list(page) {
                 let _this = this;
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
-                    page: 1,
-                    size: 1
-                }).then((respond)=>{
-                    console.log("查询大章列表结果：", respond);
-                    _this.chapters = respond.data.list;
+                    page: page,
+                    size: _this.$refs.pagination.size,
+                }).then((response)=>{
+                    console.log("查询大章列表结果：", response);
+                    _this.chapters = response.data.list;
+                    _this.$refs.pagination.render(page, response.data.total);
                 })
             }
         }
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -17,6 +17,26 @@ create table `chapter` (
     primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='大章';
 
-insert into `chapter` (id, course_id, name) value ('00000000', '00000000', '测试大章一');
-insert into `chapter` (id, course_id, name) value ('00000001', '00000001', '测试大章二');
+insert into `chapter` (id, course_id, name) value ('00000000', '00000000', '测试大章00');
+insert into `chapter` (id, course_id, name) value ('00000001', '00000000', '测试大章01');
+insert into `chapter` (id, course_id, name) value ('00000002', '00000000', '测试大章02');
+insert into `chapter` (id, course_id, name) value ('00000003', '00000000', '测试大章03');
+insert into `chapter` (id, course_id, name) value ('00000004', '00000000', '测试大章04');
+insert into `chapter` (id, course_id, name) value ('00000005', '00000000', '测试大章05');
+insert into `chapter` (id, course_id, name) value ('00000006', '00000000', '测试大章06');
+insert into `chapter` (id, course_id, name) value ('00000007', '00000000', '测试大章07');
+insert into `chapter` (id, course_id, name) value ('00000008', '00000000', '测试大章08');
+insert into `chapter` (id, course_id, name) value ('00000009', '00000000', '测试大章09');
+insert into `chapter` (id, course_id, name) value ('00000010', '00000000', '测试大章10');
+insert into `chapter` (id, course_id, name) value ('00000011', '00000000', '测试大章11');
+insert into `chapter` (id, course_id, name) value ('00000012', '00000000', '测试大章12');
+insert into `chapter` (id, course_id, name) value ('00000013', '00000000', '测试大章13');
+insert into `chapter` (id, course_id, name) value ('00000014', '00000000', '测试大章14');
+insert into `chapter` (id, course_id, name) value ('00000015', '00000000', '测试大章15');
+insert into `chapter` (id, course_id, name) value ('00000016', '00000000', '测试大章16');
+insert into `chapter` (id, course_id, name) value ('00000017', '00000000', '测试大章17');
+insert into `chapter` (id, course_id, name) value ('00000018', '00000000', '测试大章18');
+insert into `chapter` (id, course_id, name) value ('00000019', '00000000', '测试大章19');
+insert into `chapter` (id, course_id, name) value ('00000020', '00000000', '测试大章20');
+insert into `chapter` (id, course_id, name) value ('00000021', '00000000', '测试大章21');
 
