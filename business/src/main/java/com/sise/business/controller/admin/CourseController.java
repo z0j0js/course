8676becd14e0da854677394ecc8ed2e4cbbf30ed
z0j0js <783155876@qq.com>
2020-11-29 10:59:50 +0800

    1. 课程和分类关联保存和显示

--- business/src/main/java/com/sise/business/controller/admin/CourseController.java
+++ business/src/main/java/com/sise/business/controller/admin/CourseController.java
@@ -1,8 +1,7 @@
 package com.sise.business.controller.admin;
 
-import com.sise.server.dto.CourseDto;
-import com.sise.server.dto.PageDto;
-import com.sise.server.dto.ResponseDto;
+import com.sise.server.dto.*;
+import com.sise.server.service.CourseCategoryService;
 import com.sise.server.service.CourseService;
 import com.sise.server.util.ValidatorUtil;
 import org.slf4j.Logger;
@@ -10,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/course")
@@ -20,6 +20,9 @@ public class CourseController {
 
     @Resource
     private CourseService courseService;
+
+    @Resource
+    private CourseCategoryService courseCategoryService;
 
     /**
      * 列表查询
@@ -58,4 +61,17 @@ public class CourseController {
         courseService.delete(id);
         return responseDto;
     }
+
+    /**
+     * 查找课程下所有分类
+     * @param courseId
+     */
+    @PostMapping("/list-category/{courseId}")
+    public ResponseDto listCategory(@PathVariable(value = "courseId") String courseId) {
+        ResponseDto responseDto = new ResponseDto();
+        List<CourseCategoryDto> dtoList = courseCategoryService.listByCourse(courseId);
+        responseDto.setContent(dtoList);
+        return responseDto;
+    }
+
 }
