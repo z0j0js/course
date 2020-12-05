cfa3de9d9218d2d8a6f90f2cbf95abba89ca88d4
z0j0js <783155876@qq.com>
2020-12-05 16:52:15 +0800

    1. 文件上传功能开发：课程封面上传

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -94,6 +94,21 @@
                      <ul id="tree" class="ztree"></ul>
                  </div>
               </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">封面</label>
+                    <div class="col-sm-10">
+                        <file v-bind:input-id="'image-upload'"
+                              v-bind:text="'上传封面'"
+                              v-bind:after-upload="afterUpload"
+                              v-bind:use="FILE_USE.COURSE.key"
+                              v-bind:suffixs="['jpg', 'jpeg', 'png']"></file>
+                        <div v-show="course.image" class="row">
+                            <div class="col-md-6">
+                                <img v-bind:src="course.image" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
               <div class="form-group">
                 <label class="col-sm-2 control-label">名称</label>
                 <div class="col-sm-10">
@@ -124,12 +139,6 @@
                 <label class="col-sm-2 control-label">价格（元）</label>
                 <div class="col-sm-10">
                   <input v-model="course.price" class="form-control">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label">封面</label>
-                <div class="col-sm-10">
-                  <input v-model="course.image" class="form-control">
                 </div>
               </div>
               <div class="form-group">
@@ -223,8 +232,9 @@
 
 <script>
   import Pagination from "../../components/pagination";
+  import File from "../../components/file";
   export default {
-    components: {Pagination},
+    components: {Pagination, File},
     name: "business-course",
     data: function() {
       return {
@@ -233,6 +243,7 @@
         COURSE_LEVEL: COURSE_LEVEL,
         COURSE_CHARGE: COURSE_CHARGE,
         COURSE_STATUS: COURSE_STATUS,
+        FILE_USE: FILE_USE,
         categorys: [],
         tree: {},
         saveContentLabel: "",
@@ -457,6 +468,7 @@
                 }
             });
         },
+
         allTeacher() {
             let _this = this;
             Loading.show();
@@ -466,6 +478,12 @@
                 _this.teachers = resp.content;
             })
         },
+
+        afterUpload(resp) {
+            let _this = this;
+            let image = resp.content.path;
+            _this.course.image = image;
+        }
     }
   }
 </script>
