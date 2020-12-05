fb029d77aad1c17d9279f27a41682460bd066575
z0j0js <783155876@qq.com>
2020-12-05 15:35:04 +0800

    1. 文件上传功能开发：上传文件时保存文件记录

--- file/src/main/java/com/sise/file/controller/admin/UploadController.java
+++ file/src/main/java/com/sise/file/controller/admin/UploadController.java
@@ -1,6 +1,8 @@
 package com.sise.file.controller.admin;
 
+import com.sise.server.dto.FileDto;
 import com.sise.server.dto.ResponseDto;
+import com.sise.server.service.FileService;
 import com.sise.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 
@@ -27,17 +30,31 @@ public class UploadController {
     @Value("${file.path}")
     private String FILE_PATH;
 
+    @Resource
+    private FileService fileService;
+
     @RequestMapping("/upload")
     public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
         LOG.info("上传文件开始");
-        String fileName = file.getOriginalFilename();
         String key = UuidUtil.getShortUuid();
-        String fullPath = FILE_PATH + "teacher/" + key + "-" + fileName;
+        String fileName = file.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+        String path = "teacher/" + key + "." + suffix;
+        String fullPath = FILE_PATH + path;
         File dest = new File(fullPath);
         file.transferTo(dest);
 
+        LOG.info("上传文件开始");
+        FileDto fileDto = new FileDto();
+        fileDto.setPath(path);
+        fileDto.setName(fileName);
+        fileDto.setSize(Math.toIntExact(file.getSize()));
+        fileDto.setSuffix(suffix);
+        fileDto.setUse("");
+        fileService.save(fileDto);
+
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setContent(FILE_DOMAIN + "teacher/" + key + "-" + fileName);
+        responseDto.setContent(FILE_DOMAIN + path);
         return responseDto;
     }
 }
--- server/src/main/java/com/sise/server/domain/File.java
+++ server/src/main/java/com/sise/server/domain/File.java
@@ -1,75 +1,32 @@
-package com.sise.server.dto;
+package com.sise.server.domain;
 
 import java.util.Date;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
-public class FileDto {
-
-    /**
-     * id
-     */
+public class File {
     private String id;
 
-    /**
-     * 相对路径
-     */
     private String path;
 
-    /**
-     * 文件名
-     */
     private String name;
 
-    /**
-     * 后缀
-     */
     private String suffix;
 
-    /**
-     * 大小|字节B
-     */
     private Integer size;
 
-    /**
-     * 用途|枚举[FileUseEnum]：COURSE("C", "讲师"), TEACHER("T", "课程")
-     */
     private String use;
 
-    /**
-     * 创建时间
-     */
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createdAt;
 
-    /**
-     * 修改时间
-     */
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updatedAt;
 
-    /**
-     * 已上传分片
-     */
     private Integer shardIndex;
 
-    /**
-     * 分片大小|B
-     */
     private Integer shardSize;
 
-    /**
-     * 分片总数
-     */
     private Integer shardTotal;
 
-    /**
-     * 文件标识
-     */
     private String key;
 
-    /**
-     * vod|阿里云vod
-     */
     private String vod;
 
     public String getId() {
@@ -176,7 +133,6 @@ public class FileDto {
         this.vod = vod;
     }
 
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -199,5 +155,4 @@ public class FileDto {
         sb.append("]");
         return sb.toString();
     }
-
 }
