318ea6a163eabfff30baeb935d12788b210daa2c
z0j0js <783155876@qq.com>
2020-12-15 20:43:13 +0800

    1. 大文件断点续传与极速秒传：支持极速秒传

--- file/src/main/java/com/sise/file/controller/admin/UploadController.java
+++ file/src/main/java/com/sise/file/controller/admin/UploadController.java
@@ -126,7 +126,8 @@ public class UploadController {
         LOG.info("检查上传分片开始：{}", key);
         ResponseDto responseDto = new ResponseDto();
         FileDto fileDto = fileService.findByKey(key);
-//        if (fileDto != null) {
+        if (fileDto != null) {
+            fileDto.setPath(FILE_DOMAIN + fileDto.getPath());
 //            if (StringUtils.isEmpty(fileDto.getVod())) {
 //                fileDto.setPath(FILE_DOMAIN + fileDto.getPath());
 //            } else {
@@ -136,7 +137,7 @@ public class UploadController {
 //                String fileUrl = response.getMezzanine().getFileURL();
 //                fileDto.setPath(fileUrl);
 //            }
-//        }
+        }
         responseDto.setContent(fileDto);
         return responseDto;
     }
