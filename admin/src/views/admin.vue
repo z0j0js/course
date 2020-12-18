a147a74b2b8e4bd66ff522c6183162329907a3b7
z0j0js <783155876@qq.com>
2020-12-18 23:48:44 +0800

    1. 路由权限判断

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -530,12 +530,22 @@ export default {
         _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
         $.getScript('/ace/assets/js/ace.min.js');
         _this.loginUser = Tool.getLoginUser();
+
+        if (!_this.hasResourceRouter(_this.$route.name)) {
+            _this.$router.push("/login");
+        }
     },
     watch: {
         $route: {
             handler:function(val, oldVal){
                 console.log("---->页面跳转：", val, oldVal);
                 let _this = this;
+
+                if (!_this.hasResourceRouter(val.name)) {
+                    _this.$router.push("/login");
+                    return;
+                }
+
                 _this.$nextTick(function(){  //页面加载完成后执行
                     _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
                 })
@@ -543,6 +553,24 @@ export default {
         }
     },
     methods: {
+        /**
+         * 查找是否有权限
+         * @param router
+         */
+        hasResourceRouter(router) {
+            let _this = this;
+            let resources = Tool.getLoginUser().resources;
+            if (Tool.isEmpty(resources)) {
+                return false;
+            }
+            for (let i = 0; i < resources.length; i++) {
+                if (router === resources[i].page) {
+                    return true;
+                }
+            }
+            return false;
+        },
+
         /**
          * 查找是否有权限
          * @param id
