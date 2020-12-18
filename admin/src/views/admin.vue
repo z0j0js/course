77228da3490d087853c94c8cc7ed9c38760c918f
z0j0js <783155876@qq.com>
2020-12-18 23:44:11 +0800

    1. 前端界面权限拦截

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -361,7 +361,7 @@
                     <b class="arrow"></b>
                 </li>
 
-                <li class="">
+                <li v-show="hasResource('01')" class="">
                     <a href="#" class="dropdown-toggle">
                         <i class="menu-icon fa fa-list"></i>
                         <span class="menu-text"> 系统管理 </span>
@@ -372,7 +372,7 @@
                     <b class="arrow"></b>
 
                     <ul class="submenu">
-                        <li class="" id="system-user-sidebar">
+                        <li v-show="hasResource('0101')" class="" id="system-user-sidebar">
                             <router-link to="/system/user">
                                 <i class="menu-icon fa fa-caret-right"></i>
                                 用户管理
@@ -380,7 +380,7 @@
 
                             <b class="arrow"></b>
                         </li>
-                        <li class="" id="system-resource-sidebar">
+                        <li v-show="hasResource('0102')" class="" id="system-resource-sidebar">
                             <router-link to="/system/resource">
                                 <i class="menu-icon fa fa-caret-right"></i>
                                 资源管理
@@ -388,7 +388,7 @@
 
                             <b class="arrow"></b>
                         </li>
-                        <li class="" id="system-role-sidebar">
+                        <li v-show="hasResource('0103')" class="" id="system-role-sidebar">
                             <router-link to="/system/role">
                                 <i class="menu-icon fa fa-caret-right"></i>
                                 角色管理
@@ -400,7 +400,7 @@
                     </ul>
                 </li>
 
-                <li class="">
+                <li v-show="hasResource('02')" class="">
                     <a href="#" class="dropdown-toggle">
                         <i class="menu-icon fa fa-list"></i>
                         <span class="menu-text"> 业务管理 </span>
@@ -411,7 +411,7 @@
                     <b class="arrow"></b>
 
                     <ul class="submenu">
-                        <li class="" id="business-category-sidebar">
+                        <li v-show="hasResource('0201')" class="" id="business-category-sidebar">
                             <router-link to="/business/category">
                                 <i class="menu-icon fa fa-caret-right"></i>
                                 分类管理
@@ -419,7 +419,7 @@
 
                             <b class="arrow"></b>
                         </li>
-                        <li class="" id="business-course-sidebar">
+                        <li v-show="hasResource('0202')" class="" id="business-course-sidebar">
                             <router-link to="/business/course">
                                 <i class="menu-icon fa fa-caret-right"></i>
                                 课程管理
@@ -427,7 +427,7 @@
 
                             <b class="arrow"></b>
                         </li>
-                        <li class="" id="business-teacher-sidebar">
+                        <li v-show="hasResource('0203')" class="" id="business-teacher-sidebar">
                             <router-link to="/business/teacher">
                                 <i class="menu-icon fa fa-caret-right"></i>
                                 讲师管理
@@ -439,7 +439,7 @@
                     </ul>
                 </li>
 
-                <li class="">
+                <li v-show="hasResource('03')" class="">
                     <a href="#" class="dropdown-toggle">
                         <i class="menu-icon fa fa-list"></i>
                         <span class="menu-text"> 文件管理 </span>
@@ -450,7 +450,7 @@
                     <b class="arrow"></b>
 
                     <ul class="submenu">
-                        <li class="" id="file-file-sidebar">
+                        <li v-show="hasResource('0301')" class="" id="file-file-sidebar">
                             <router-link to="/file/file">
                                 <i class="menu-icon fa fa-caret-right"></i>
                                 文件管理
@@ -543,6 +543,14 @@ export default {
         }
     },
     methods: {
+        /**
+         * 查找是否有权限
+         * @param id
+         */
+        hasResource(id) {
+            return Tool.hasResource(id);
+        },
+
         login() {
             this.$router.push("/admin")
         },
