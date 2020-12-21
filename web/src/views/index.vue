fa486d2b74259da1ac443cbf466d88a3aa51d626
z0j0js <783155876@qq.com>
2020-12-21 20:38:26 +0800

    1. 首页开发——显示新出课程

--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -15,17 +15,21 @@
             <div class="container">
 
                 <div class="row">
-                    <div class="col-md-4">
+                    <div v-for="o in news" class="col-md-4">
                         <div class="card mb-4 box-shadow">
-                            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                            <img class="img-fluid" v-bind:src="o.image">
                             <div class="card-body">
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <h4 class="">{{o.name}}</h4>
+                                <p class="card-text">{{o.summary}}</p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
                                         <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                                         <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                                     </div>
-                                    <small class="text-muted">9 mins</small>
+                                    <div class="text-muted">
+                                        <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{o.price}}</span>&nbsp;
+                                        <span class="badge badge-info"><i class="fa fa-user" aria-hidden="true"></i>&nbsp;123</span>&nbsp;
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -160,7 +164,52 @@
 </template>
 
 <script>
+    // import TheCourse from "../components/the-course";
     export default {
-        name : 'index',
+        name: 'index',
+        // components: {TheCourse},
+        data: function () {
+            return {
+                news: [],
+            }
+        },
+        mounted() {
+            let _this = this;
+            _this.listNew();
+        },
+        methods: {
+            /**
+             * 查询新上好课
+             */
+            listNew() {
+                let _this = this;
+                _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/web/course/list-new').then((response)=>{
+                    console.log("查询新上好课结果：", response);
+                    let resp = response.data;
+                    if (resp.success) {
+                        _this.news = resp.content;
+                    }
+                }).catch((response)=>{
+                    console.log("error：", response);
+                })
+            },
+        }
     }
 </script>
+
+<style>
+    .title1{
+        margin-bottom: 2rem;
+        color: #fafafa;
+        letter-spacing: 0;
+        text-shadow: 0px 1px 0px #999, 0px 2px 0px #888, 0px 3px 0px #777, 0px 4px 0px #666, 0px 5px 0px #555, 0px 6px 0px #444, 0px 7px 0px #333, 0px 8px 7px #001135;
+        font-size: 2rem;
+    }
+    .title2{
+        margin-bottom: 2rem;
+        color: transparent;
+        -webkit-text-stroke: 1px black;
+        letter-spacing: 0.04em;
+        font-size: 2rem;
+    }
+</style>
