7d090eff4382a4593fe8da091f8e3db309882701
z0j0js <783155876@qq.com>
2020-12-22 13:42:02 +0800

    1. 课程详情页面开发

--- web/src/router.js
+++ web/src/router.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Index from "./views/index.vue"
 import List from "./views/list.vue"
-// import Detail from "./views/detail.vue"
+import Detail from "./views/detail.vue"
 
 Vue.use(Router);
 
@@ -18,10 +18,8 @@ export default new Router({
     }, {
         path: "/list",
         component: List
-    },
-    //     {
-    //     path: "/detail",
-    //     component: Detail
-    // }
-    ]
+    }, {
+        path: "/detail",
+        component: Detail
+    }]
 })
